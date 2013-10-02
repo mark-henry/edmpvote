3dc16057a23812302b2513cf13940f3809210c3b
Mark Henry <theratcheteer+github@gmail.com>
2013-10-01 21:41:49 -0700

    CSS and HTML fully GAE-ified

--- vote.py
+++ vote.py
@@ -13,12 +13,19 @@ from google.appengine.ext import ndb
 import cgi
 import jinja2, webapp2
 
-SCORE_RANGE = 5
+SCORE_RANGE = 9
 DEBUG_MODE = True
 
 JINJA_ENVIRONMENT = jinja2.Environment(
    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
    extensions=['jinja2.ext.autoescape'])
+
+example_template_vars = {
+      'entries': [{'redditname':'mark-henry', 'title':'Track Title', 'url':'https://soundcloud.com/mark-henry/cadavre-0'}],
+      'uservotes': {'mark-henry':4},
+      'votingclosed': False,
+      'SCORE_RANGE': SCORE_RANGE,
+    }
 
 def err(args):
    logging.error("\n>>DEBUG: %s" % args)
@@ -29,12 +36,7 @@ client_ip = os.getenv("REMOTE_ADDR")
 class VotePage(webapp2.RequestHandler):
   def get(self):
     template = JINJA_ENVIRONMENT.get_template('vote.html')
-    template_vars = {
-      'entries': [{'redditname':'mark-henry', 'title':'Track Title', 'url':'https://soundcloud.com/mark-henry/cadavre-0'}],
-      'session': {'asdf':9},
-      'votingclosed': False,
-      'SCORE_RANGE': SCORE_RANGE,
-    }
+    template_vars = example_template_vars
     self.response.write(template.render(template_vars))
 
 # Define webapp2 application
