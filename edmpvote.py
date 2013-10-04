1b0a02cfbf0c4c49e0eb29cb6cf0d1a4a0e2bbe2
Mark Henry <theratcheteer+github@gmail.com>
2013-10-04 11:03:30 -0700

    Implemented default poll, credited rxi for original pages

--- edmpvote.py
+++ edmpvote.py
@@ -1,9 +1,21 @@
-# Datastore schema
+# Datastore models and other common stuff
 
 __author__ = '/u/mark-henry'
 
-from google.appengine.api import users
+import os, logging
+
 from google.appengine.ext import ndb
+
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+  extensions=['jinja2.ext.autoescape'])
+
+DEBUG_MODE = True
+
+def err(args):
+  logging.error("\n>>DEBUG: %s" % args)
 
 class Entry(ndb.Model):
   """Models a competition entry"""
@@ -26,3 +38,20 @@ class Poll(ndb.Model):
   entries = ndb.KeyProperty(kind=Entry, repeated=True)
   voting_enabled = ndb.BooleanProperty(required=True, default=False)
   end_date = ndb.DateTimeProperty()
+
+class DefaultPoll(ndb.Model):
+  """A single DefaultPoll instance contains a reference to the default poll, which is displayed
+      by the vote page when no poll is specified."""
+  default_poll_key = ndb.KeyProperty(kind=Poll)
+
+def getDefaultPollObject():
+  """Returns an object containing a reference to the default poll"""
+  query_results = DefaultPoll.query().fetch()
+  if query_results:
+    return query_results[0]
+  else:
+    # Initialize the default poll object if it does not exist
+    err("initted deef poll")
+    default_poll = DefaultPoll()
+    default_poll.put()
+    return default_poll
--- vote.py
+++ vote.py
@@ -1,44 +1,31 @@
 # Voting page by /u/mark-henry for /r/edmproduction
-# Original voting page by /u/mwd410
+# Based on original voting page by /u/rxi
 
 __author__  = "/u/mark-henry"
 
 import os
 import urllib2
-import logging
 import re
 
-from google.appengine.api import users
 from google.appengine.ext import ndb
 
 import cgi
 import jinja2, webapp2
 
-SCORE_RANGE = 9
-DEBUG_MODE = True
+import edmpvote
 
-JINJA_ENVIRONMENT = jinja2.Environment(
-   loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-   extensions=['jinja2.ext.autoescape'])
+SCORE_RANGE = 10
 
-example_template_vars = {
-      'entries': [{'entryid':'mark-henry', 'title':'Track Title', 'author':'mark-henry', 'url':'http://soundcloud.com/mark-henry/cadavre-0'}],
-      'uservotes': {'mark-henry':4},
-      'votingclosed': False,
-      'SCORE_RANGE': SCORE_RANGE,
-    }
-
-def err(args):
-   logging.error("\n>>DEBUG: %s" % args)
-
-def getSessionID():
+def getVoterID():
   '''Gets a string that identifies the voter.'''
   # Right now it's just their IP address
   client_ip = os.getenv("REMOTE_ADDR")
   return client_ip
 
 def getTitle(soundcloudurl):
-  '''Gets the title for given track on soundcloud'''
+  '''Gets the title for SoundCloud track specified by url'''
+  # Currently implemented as a ghetto API
+  #   that strips the title from the song's page's HTML with a regex
   res = urllib2.urlopen(soundcloudurl)
   content = res.read().decode(encoding='UTF-8')
   pattern = '<meta content="([^<]+?)" property="og:title" />'
@@ -46,21 +33,31 @@ def getTitle(soundcloudurl):
   if match:
     return match.group(1)
   else:
-    return "couldn't find it in " + content
+    return "err"
 
-def getDefaultPoll():
-  '''Gets the poll that should be displayed when no poll is specified in query string'''
-  return 0
+example_template_vars = {
+      'entries': [{'entryid':'mark-henry',
+                    'title':getTitle('http://soundcloud.com/mark-henry/cadavre-0'),
+                    'author':'mark-henry',
+                    'url':'http://soundcloud.com/mark-henry/cadavre-0'}],
+      'uservotes': {'mark-henry':4},
+      'votingclosed': False,
+      'SCORE_RANGE': SCORE_RANGE,
+    }
 
 class VotePage(webapp2.RequestHandler):
-  def get(self):
-    template = JINJA_ENVIRONMENT.get_template('vote.html')
-    template_vars = example_template_vars
-    template_vars['entries'][0]['title'] = getTitle('http://soundcloud.com/mark-henry/cadavre-0')
+  def showPoll(self, pollKey):
+    poll = pollKey.get()
+    template_vars = {}
+    template = edmpvote.JINJA_ENVIRONMENT.get_template('vote.html')
     self.response.write(template.render(template_vars))
 
+  def get(self):
+    queries = self.request.GET
+    if 'poll' in queries:
+      self.showPoll(ndb.Key(urlsafe=queries['poll']))
+    else:
+      self.showPoll(edmpvote.getDefaultPollObject().default_poll_key)
+
 # Define webapp2 application
-directory = [
-  ('/vote', VotePage),
-]
-application = webapp2.WSGIApplication(directory, debug=DEBUG_MODE)
+application = webapp2.WSGIApplication([('/vote', VotePage)], debug=edmpvote.DEBUG_MODE)
