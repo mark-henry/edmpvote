8c53a40d7561bb63b6d9e16b808ed93906f5ff65
Mark Henry <markhenry.software@gmail.com>
2015-04-14 09:54:03 -0700

    Indentation, mostly

--- admin.py
+++ admin.py
@@ -1,113 +1,113 @@
 __author__ = '/u/mark-henry'
 
-import os
-
 from edmpvote import *
 
 from google.appengine.ext import ndb
 
-import jinja2, webapp2, logging
+import webapp2
+import logging
+import datetime
 
 
 def makePollResults(entries, ballots):
-  """Renders a sorted list of standings"""
+    """Renders a sorted list of standings"""
 
-  # Collect the votes
-  votes_by_user = {}
-  for entry in entries:
-    votes_by_user.update({entry.author: []})
-  for ballot in ballots:
-    for vote in ballot.votes:
-      if (votes_by_user.has_key(vote.entryid)): 
-        votes_by_user[vote.entryid].append(vote.value)
-        
-  # Average the scores
-  standings = []
-  for user in votes_by_user.keys():
-    if len(votes_by_user[user]) == 0:
-      score = "0"
-    else:
-      ratio = sum(votes_by_user[user]) / float(len(votes_by_user[user]))
-      score = str(round(ratio, 2))
-    standings.append(score + " " + user)
+    # Collect the votes
+    votes_by_user = {}
+    for entry in entries:
+        votes_by_user.update({entry.author: []})
+    for ballot in ballots:
+        for vote in ballot.votes:
+            if vote.entryid in votes_by_user:
+                votes_by_user[vote.entryid].append(vote.value)
 
-  sortedstandings = sorted(standings, reverse=True)
+    # Average the scores
+    scores = []
+    for user in votes_by_user.keys():
+        if len(votes_by_user[user]) == 0:
+            score = "0"
+        else:
+            ratio = sum(votes_by_user[user]) / float(len(votes_by_user[user]))
+            score = str(round(ratio, 2))
+        scores.append(score + " " + user)
 
-  for i in range(len(sortedstandings)):
-    sortedstandings[i] = str(i+1) + ". " + sortedstandings[i]
+    sortedstandings = []
+    for i, standing in enumerate(sorted(scores, reverse=True)):
+        sortedstandings.append(str(i + 1) + ".  " + standing)
 
-  return "\n".join(sortedstandings)
+    return "\n".join(sortedstandings)
 
 
 class AdminPage(webapp2.RequestHandler):
-  def pollsList(self):
-    # Fetch data
-    queries = self.request.GET
-    default_poll_object = getDefaultPollObject()
-    polls = Poll.query().fetch()
-    if not polls: polls = []
+    def pollsList(self):
+        # Fetch data
+        queries = self.request.GET
+        default_poll_object = getDefaultPollObject()
+        polls = Poll.query().fetch() or []
+        polls.sort(reverse=True, key=lambda p: p.created or datetime.datetime.min)
 
-    # Update data
-    if 'default' in queries:
-      # Update the default poll value
-      default_poll_object.default_poll_key = ndb.Key(urlsafe=queries['default'])
-      default_poll_object.put()
+        # Update data
+        if 'default' in queries:
+            # Update the default poll value
+            default_poll_object.default_poll_key = ndb.Key(urlsafe=queries['default'])
+            default_poll_object.put()
 
-    # Render
-    template_vars = {'polls':polls, 'default_poll_key':default_poll_object.default_poll_key}
-    template = JINJA_ENVIRONMENT.get_template('polls-list.html')
-    self.response.write(template.render(template_vars))
+        # Render
+        template_vars = {'polls': polls, 'default_poll_key': default_poll_object.default_poll_key}
+        template = JINJA_ENVIRONMENT.get_template('polls-list.html')
+        self.response.write(template.render(template_vars))
 
-  def editPoll(self, poll_key, isPost):
-    if(isPost):
-      queries = self.request.POST
-    else:
-      queries = self.request.GET
-    poll = poll_key.get()
-    
-    logging.info('get some editPoll')
-    if 'title' in queries:
-      logging.info('editting title ' + queries['title'])
-      poll.title = queries['title']
-      poll.put()
-    if 'voting' in queries:
-      if queries['voting'] == 'enable':
-        poll.voting_enabled = True
-      elif queries['voting'] == 'disable':
-        poll.voting_enabled = False
-      poll.put()
-    if 'author' in queries and 'url' in queries:
-      Entry(author=queries['author'], url=queries['url'], parent=poll_key).put()
+    def editPoll(self, poll_key, is_post):
+        if is_post:
+            queries = self.request.POST
+        else:
+            queries = self.request.GET
+        poll = poll_key.get()
 
-    entries = Entry.query(ancestor=poll_key).fetch()
-    ballots = Ballot.query(ancestor=poll_key).fetch()
-    results = makePollResults(entries, ballots)
+        logging.info('get some editPoll')
+        if 'title' in queries:
+            logging.info('editting title ' + queries['title'])
+            poll.title = queries['title']
+            poll.put()
+        if 'voting' in queries:
+            if queries['voting'] == 'enable':
+                poll.voting_enabled = True
+            elif queries['voting'] == 'disable':
+                poll.voting_enabled = False
+            poll.put()
+        if 'author' in queries and 'url' in queries:
+            Entry(author=queries['author'], url=queries['url'], parent=poll_key).put()
 
-    template = JINJA_ENVIRONMENT.get_template('single-poll.html')
-    template_vars = {'poll':poll, 'entries':entries, 'ballots':ballots, 'results':results}
-    self.response.write(template.render(template_vars))
-  
-  def newPoll(self):
-    new_poll = Poll(title='New Poll')
-    new_poll.put()
-    self.editPoll(new_poll.key, True)
+        entries = Entry.query(ancestor=poll_key).fetch()
+        ballots = Ballot.query(ancestor=poll_key).fetch()
+        results = makePollResults(entries, ballots)
 
-  def get(self):
-    self.parseRequest(self.request.GET)
-	  
-  def post(self):
-    self.parseRequest(self.request.POST)
-      
-  def parseRequest(self, queries):
-    if 'delete' in queries:
-      ndb.Key(urlsafe=queries['delete']).delete()
-    if 'action' in queries and queries['action'] == 'newpoll':
-      self.newPoll()
-    elif 'poll' in queries:
-      self.editPoll(ndb.Key(urlsafe=queries['poll']), True)
-    else:
-      self.pollsList()
+        template = JINJA_ENVIRONMENT.get_template('single-poll.html')
+        template_vars = {'poll': poll, 'entries': entries, 'ballots': ballots, 'results': results}
+        self.response.write(template.render(template_vars))
+
+    def newPoll(self):
+        new_poll = Poll(title='New Poll')
+        new_poll.put()
+        self.editPoll(new_poll.key, True)
+
+    def get(self):
+        self.parseRequest(self.request.GET)
+
+    def post(self):
+        self.parseRequest(self.request.POST)
+
+    def parseRequest(self, queries):
+        if 'delete' in queries:
+            ndb.Key(urlsafe=queries['delete']).delete()
+        if 'action' in queries and queries['action'] == 'newpoll':
+            self.newPoll()
+        elif 'poll' in queries:
+            self.editPoll(ndb.Key(urlsafe=queries['poll']), True)
+        else:
+            self.pollsList()
+
 
 application = webapp2.WSGIApplication(
-    [('/admin.*', AdminPage)], 
+    [('/admin.*', AdminPage)],
     debug=DEBUG_MODE)
--- edmpvote.py
+++ edmpvote.py
@@ -2,56 +2,65 @@
 
 __author__ = '/u/mark-henry'
 
-import os, logging
+import os
+import logging
 
 from google.appengine.ext import ndb
 
 import jinja2
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-  extensions=['jinja2.ext.autoescape'])
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'])
 
 DEBUG_MODE = False
 SCORE_RANGE = 10
 
+
 def err(args):
-  logging.error("\n>>DEBUG: %s" % args)
+    logging.error("\n>>DEBUG: %s" % args)
+
 
 class Entry(ndb.Model):
-  """Models a competition entry"""
-  author = ndb.StringProperty(required=True)
-  url = ndb.StringProperty(required=True)
+    """Models a competition entry"""
+    author = ndb.StringProperty(required=True)
+    url = ndb.StringProperty(required=True)
+
 
 class Vote(ndb.Model):
-  """Models a entryid-value pair"""
-  entryid = ndb.StringProperty('i', required=True)
-  value = ndb.IntegerProperty('v', required=True)
+    """Models a entryid-value pair"""
+    entryid = ndb.StringProperty('i', required=True)
+    value = ndb.IntegerProperty('v', required=True)
+
 
 class Ballot(ndb.Model):
-  """Models a user's votes for a specific competition"""
-  voterid = ndb.StringProperty(required=True)
-  votes = ndb.StructuredProperty(Vote, repeated=True)
+    """Models a user's votes for a specific competition"""
+    voterid = ndb.StringProperty(required=True)
+    votes = ndb.StructuredProperty(Vote, repeated=True)
+
 
 class Poll(ndb.Model):
-  """Models a poll containing entries and votes"""
-  title = ndb.StringProperty(required=True)
-  voting_enabled = ndb.BooleanProperty(required=True, default=False)
-  end_date = ndb.DateTimeProperty()
+    """Models a poll containing entries and votes"""
+    title = ndb.StringProperty(required=True)
+    voting_enabled = ndb.BooleanProperty(required=True, default=False)
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    end_date = ndb.DateTimeProperty()
+
 
 class DefaultPoll(ndb.Model):
-  """A single DefaultPoll instance contains a reference to the default poll, which is displayed
+    """A single DefaultPoll instance contains a reference to the default poll, which is displayed
       by the vote page when no poll is specified."""
-  default_poll_key = ndb.KeyProperty(kind=Poll)
+    default_poll_key = ndb.KeyProperty(kind=Poll)
+
 
 def getDefaultPollObject():
-  """Returns an object containing a reference to the default poll"""
-  query_results = DefaultPoll.query().fetch()
-  if query_results:
-    return query_results[0]
-  else:
-    # Initialize the default poll object if it does not exist
-    err("initted deef poll")
-    default_poll = DefaultPoll()
-    default_poll.put()
-    return default_poll
+    """Returns an object containing a reference to the default poll"""
+    query_results = DefaultPoll.query().fetch()
+    if query_results:
+        return query_results[0]
+    else:
+        # Initialize the default poll object if it does not exist
+        err("initted deef poll")
+        default_poll = DefaultPoll()
+        default_poll.put()
+        return default_poll
