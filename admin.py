67b69030f8763bb0632cbc03da9efcf5251da7db
Mark <mark.henry@salesforce.com>
2016-12-31 10:14:00 -0800

    Version 6
    Changed voting normalization scheme
    
    Squashed commit of the following:
    
    commit ecdb268f13b71feb7f0e83043c42f2d9397afed4
    Author: Mark <mark.henry@salesforce.com>
    Date:   Fri Dec 30 16:03:20 2016 -0800
    
        Normalize votes from 1-5 per voter
    
    commit 6fab145f89c49c2b23cdc8d4e7424c7847577e00
    Author: Mark <mark.henry@salesforce.com>
    Date:   Fri Dec 30 15:27:00 2016 -0800
    
        Filter out ballots with one or fewer votes
    
    commit 2261d241901ed54df9fe439ca325afa76ed29507
    Author: Mark <mark.henry@salesforce.com>
    Date:   Fri Dec 30 14:49:41 2016 -0800
    
        Redirect user to correct url upon creating new poll
    
    commit 5026a9dcc72a70a0b6d96c98b07fe6e76622b80a
    Author: Mark <mark.henry@salesforce.com>
    Date:   Wed Jun 29 11:01:34 2016 -0700
    
        Add quickadd feature
    
    commit b26efc758ec5692f5161913875aa97deb470f9b0
    Author: Mark <mark.henry@salesforce.com>
    Date:   Tue Jun 28 13:30:09 2016 -0700
    
        Update to SC api v3.1.2
    
    commit 3dde7d0974484942471f105f140fa331b137cbd1
    Author: Mark <mark.henry@salesforce.com>
    Date:   Tue Jun 28 13:16:00 2016 -0700
    
        Load scripts over https
    
    commit 367740870d60c186c7301fc39d6be0b7c144d5be
    Author: Mark Henry <markhenry.software@gmail.com>
    Date:   Tue Apr 14 12:22:02 2015 -0700
    
        Added endpoint for bot
    
    commit bd103d1ccd483adcbc1ff22164947ddbba6097a9
    Author: Mark Henry <markhenry.software@gmail.com>
    Date:   Tue Apr 14 12:21:10 2015 -0700
    
        Bootstrap for everyone
    
    commit 8c53a40d7561bb63b6d9e16b808ed93906f5ff65
    Author: Mark Henry <markhenry.software@gmail.com>
    Date:   Tue Apr 14 09:54:03 2015 -0700
    
        Indentation, mostly

--- admin.py
+++ admin.py
@@ -13,20 +13,30 @@ import re
 def makePollResults(entries, ballots):
     """Renders a sorted list of standings"""
 
-    # Collect the votes
+    # Filter out the single-entry ballots
+    ballots = filter(lambda ballot: len(ballot.votes) > 1, ballots)
+
+    # Group the votes by userid
     votes_by_user = {}
     for entry in entries:
         votes_by_user.update({entry.author: []})
     for ballot in ballots:
+        # Normalize each ballot values to a 1-5 scale
+        values = [vote.value for vote in ballot.votes]
+        if min(values) == max(values):
+            continue  # Ballots which express no preference do not count
+        translate = min(values)
+        scale = 4.0 / (max(values) - min(values))
         for vote in ballot.votes:
+            scaled_value = ((vote.value - translate) * scale) + 1
             if vote.entryid in votes_by_user:
-                votes_by_user[vote.entryid].append(vote.value)
+                votes_by_user[vote.entryid].append(scaled_value)
 
     # Average the scores
     scores = []
     for user in votes_by_user.keys():
         if len(votes_by_user[user]) == 0:
-            score = "0"
+            score = "(no votes)"
         else:
             ratio = sum(votes_by_user[user]) / float(len(votes_by_user[user]))
             score = str(round(ratio, 2))
@@ -108,7 +118,7 @@ class AdminPage(webapp2.RequestHandler):
     def newPoll(self):
         new_poll = Poll(title='New Poll')
         new_poll.put()
-        self.editPoll(new_poll.key)
+        return self.redirect("/admin?poll=" + new_poll.key.urlsafe())
 
     def get(self):
         self.parseRequest(self.request.GET)
